42bb2528399d60c9c48a5e44fbc2ee540144e293
Mo Ansari <paytakhtdota@gmail.com>
2024-01-01 14:19:08 -0800

    improve codes and it is clean now

--- script.js
+++ script.js
@@ -1,59 +1,58 @@
-let checkBtn = document.querySelectorAll("div .btn-success");
-let delBtn = document.querySelectorAll("div .btn-danger");
+
 const addBtn = document.querySelector("button#addBTN");
 const addBar = document.querySelector("input#addBar");
 const myList = document.querySelector("ul#tl");
 const searchBar = document.querySelector("input#searchBar")
-let searchItem = null;
-
-
+let searchItem;
 
 let tasks = [];
 
-addBtn.addEventListener("click", (e) => {
-    let newTask = addBar.value;
-    tasks.push(newTask);
+addBtn.addEventListener("click", (e) => {   // a trigger for add new task to Tasks array and start render();
+    tasks.push(addBar.value);
     addBar.value = null;
-    myList.innerHTML = null;
-    render();
+    render(tasks);
 })
 
 searchBar.addEventListener("input", e => {
     searchItem = null;
-    searchItem = e.target.value;
-    if(searchItem){
-    myList.innerHTML = null;
-    search(searchItem)};
-    if(searchItem==""){render()}
+    serachItem = e.target.value;
+    search(tasks,serachItem);
   });
-searchBar.addEventListener("input", e => {
-    if(e.target.value == null){render();console.log("null")}
-})
 
+function render(data) {  // Function to render new row with client Data
 
+    myList.innerHTML = "";  // make empty myList ul#tl before add new HTML codes
+    data.forEach((element,index) => {
+    const listItemDiv = document.createElement("div");
+    listItemDiv.classList.add("row","btn-group","list-group-horizontal","p-1");
 
-function render() {
-    tasks.forEach(element => {
-        
-        myList.innerHTML +=`<div class="row btn-group list-group-horizontal p-1">
-        <li class="list-group-item col-md-8">${element}</li>
-        <button btn-id="sbtn" type="button" class="btn btn-success col-md-2"><i class="fa-solid fa-check"></i></button>
-        <button btn-id="dbtn" type="button" class="btn btn-danger col-md-2"><i class="fa-solid fa-trash"></i></button>
-    </div>
-`
+    const listItemText = document.createElement("li");
+    listItemText.classList.add("list-group-item","col-sm-8",);
+    listItemText.innerText = element;
+    listItemDiv.appendChild(listItemText);
 
+    const checkBtn = document.createElement("button");
+    checkBtn.classList.add("btn","btn-success","col-sm-2");
+    checkBtn.dataset.id = index;
+    checkBtn.textContent = "✓";
+    listItemDiv.appendChild(checkBtn);
+
+    const delBtn = document.createElement("button");
+    delBtn.classList.add("btn","btn-danger","col-sm-2");
+    delBtn.dataset.id = index;
+    delBtn.textContent = "✕";
+    listItemDiv.appendChild(delBtn);
+
+    myList.appendChild(listItemDiv);   //append Div to ul
     })
 }
 
-function search(searchElement){
-tasks.forEach(element=>{
-        if(element.includes(searchElement)){
-            myList.innerHTML +=`<div class="row btn-group list-group-horizontal p-1">
-        <li class="list-group-item col-md-8">${element}</li>
-        <button btn-id="sbtn" type="button" class="btn btn-success col-md-2"><i class="fa-solid fa-check"></i></button>
-        <button btn-id="dbtn" type="button" class="btn btn-danger col-md-2"><i class="fa-solid fa-trash"></i></button>
-    </div>
-`
-        }
+function search(array,searchElement){
+    let matchItems=[];
+    array.forEach(element=>{
+     if(element.includes(searchElement)){
+        matchItems.push(element);
+        render(matchItems);
+    }
 })
 }
