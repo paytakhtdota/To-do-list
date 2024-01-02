50c7bb3bb85208e9dbce1430af7523d57ec34835
Mo Ansari <paytakhtdota@gmail.com>
2024-01-01 22:11:17 -0800

    added features and fixed some bugs. a few comment added to codes.A readMe.txt added

--- script.js
+++ script.js
@@ -4,55 +4,93 @@ const addBar = document.querySelector("input#addBar");
 const myList = document.querySelector("ul#tl");
 const searchBar = document.querySelector("input#searchBar")
 let searchItem;
+let doneCounter = 0; // count when click on checkBtn
 
-let tasks = [];
+let tasks = []; // an array to store the tasks
 
 addBtn.addEventListener("click", (e) => {   // a trigger for add new task to Tasks array and start render();
     tasks.push(addBar.value);
     addBar.value = null;
     render(tasks);
+
 })
 
-searchBar.addEventListener("input", e => {
+searchBar.addEventListener("input", e => {  // event lisener for search Bar; it will trigger evry time the value of input change
     searchItem = null;
-    serachItem = e.target.value;
-    search(tasks,serachItem);
-  });
+    searchItem = e.target.value;
+    search(tasks, searchItem);
+});
+
+function search(array, searchElement) {  // a function to find any tasks that match to search result 
+    let result= [];
+    if (searchBar.value.length == 0){render(tasks)}else{ // if search bar is empty no need to search! and after earse litter from input it should show tasks again!
+        // esle; in put is not empty so now we start search process
+    array.forEach(element => {  // check to 
+        if (element.includes(searchElement)) {
+            result.push(element);
+        }
+    })
+
+    if(result.length == 0){
+        myList.innerHTML = "";
+        const noMatch = document.createElement("div");
+        noMatch.classList.add("alert","alert-danger");
+        noMatch.innerHTML = " <strong>No Match Item </strong> Unfortunately, we were unable to find the item you searched for."
+        myList.appendChild(noMatch);
+
+
+    }else{render(result);}
+}
+}
+     //render the results of search
+
 
 function render(data) {  // Function to render new row with client Data
 
     myList.innerHTML = "";  // make empty myList ul#tl before add new HTML codes
-    data.forEach((element,index) => {
-    const listItemDiv = document.createElement("div");
-    listItemDiv.classList.add("row","btn-group","list-group-horizontal","p-1");
+    document.querySelector("header div span.bg-success").innerText = "Compeleted : " + doneCounter;
+    document.querySelector("header div span.bg-warning").innerText = "In Progress : " + tasks.length;
 
-    const listItemText = document.createElement("li");
-    listItemText.classList.add("list-group-item","col-sm-8",);
-    listItemText.innerText = element;
-    listItemDiv.appendChild(listItemText);
+    data.forEach((element, index) => {  //forEach for create a li for each of element in the array
+        const listItemDiv = document.createElement("div");
+        listItemDiv.classList.add("row", "btn-group", "list-group-horizontal", "p-1");
 
-    const checkBtn = document.createElement("button");
-    checkBtn.classList.add("btn","btn-success","col-sm-2");
-    checkBtn.dataset.id = index;
-    checkBtn.textContent = "✓";
-    listItemDiv.appendChild(checkBtn);
+        const listItemText = document.createElement("li");
+        listItemText.classList.add("list-group-item", "col-sm-8",);
+        listItemText.innerText = element;
+        listItemDiv.appendChild(listItemText);
 
-    const delBtn = document.createElement("button");
-    delBtn.classList.add("btn","btn-danger","col-sm-2");
-    delBtn.dataset.id = index;
-    delBtn.textContent = "✕";
-    listItemDiv.appendChild(delBtn);
+        const checkBtn = document.createElement("button");
+        checkBtn.classList.add("btn", "btn-success", "col-sm-2");
+        checkBtn.dataset.id = index;     // assign an id to each bottun to handel it latter bia Event listener 
+        checkBtn.innerHTML = `<i class="fa-regular fa-circle-check"></i>`;
+        listItemDiv.appendChild(checkBtn);
 
-    myList.appendChild(listItemDiv);   //append Div to ul
+        const delBtn = document.createElement("button");
+        delBtn.classList.add("btn", "btn-danger", "col-sm-2");
+        delBtn.dataset.id = index;   // assign an id to each bottun to handel it latter bia Event listener 
+        delBtn.innerHTML = `<i class="fa-regular fa-trash-can"></i>`;
+        listItemDiv.appendChild(delBtn);
+
+        myList.appendChild(listItemDiv);   //append Div to ul
+
+        checkBtn.addEventListener("click", jobDone)
+        delBtn.addEventListener("click", cancelTask)
+
+
     })
 }
 
-function search(array,searchElement){
-    let matchItems=[];
-    array.forEach(element=>{
-     if(element.includes(searchElement)){
-        matchItems.push(element);
-        render(matchItems);
-    }
-})
+
+
+
+function jobDone(event) {   // to handel bottun for remove an Item from list
+    tasks.splice(event.target.dataset.id, 1);
+    doneCounter++;
+    render(tasks);
+}
+
+function cancelTask(event) {    // to handel bottun for remove an Item from list
+    tasks.splice(event.target.dataset.id, 1);
+    render(tasks);
 }
